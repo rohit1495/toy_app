7f552808e73d11ecd3db99f062134c4323dd3ff0
rohit1495 <rohit.singh@nestaway.com>
2018-10-01 11:09:22 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToyApp::Application.config.secret_token = 'e47347ddee2dff257f7b957f136f753daafb0f2ca42bc545043f3f088d1cacbe28109f9aa4cf642cadffe8788918b49a0b63c41f0e5b43525d635792ee24a255'
